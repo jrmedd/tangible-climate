4f7d7b3ed0ef4776719559a1a7731dbfc45c1c5e
James Medd <james.medd@sparck.io>
2023-11-09 10:55:10 +0000

    Add some sugar

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import axiosThrottle from 'axios-request-throttle';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import LoadingButton from '@mui/lab/LoadingButton';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import {Box, Button, Card, CardActions, CardContent, CardHeader, Collapse, IconButton, Link, List, ListItem, Switch, Typography, debounce } from '@mui/material';
-import { HelpOutline } from '@mui/icons-material';
+import {Box, Button, Card, CardActions, CardContent, CardHeader, Chip, CircularProgress, Collapse, Divider, IconButton, InputAdornment, Link, List, ListItem, Switch, Typography } from '@mui/material';
+import { Flight, FlightLand, FlightTakeoff, HelpOutline } from '@mui/icons-material';
 import styled, { css } from 'styled-components';
 import { Stack } from '@mui/system';
 import sources from './sources.json'
+
+axiosThrottle.use(axios, { requestsPerSecond: 1 });
 
 const StyledMain = styled.main(props => css`
 width: 100%;
 display: flex;
 flex-flow: column;
-gap: 2rem;
 align-items: center;
 margin: 1rem 0;
 `)
 
 const ReadableParagraph = styled.p(props => css`
 width: 65ch;
-max-width: 100%;
+max-width: 95%;
+margin-bottom: 1.5rem;
 `)
 
 const BoldText = styled.span(props => css`
@@ -32,7 +35,7 @@ background-color: #ededed;
 const StyledSection = styled.section(props => css`
 flex-flow: column;
 gap: 1rem;
-padding: 1rem;
+padding: 0 1rem;
 opacity: ${props.isVisible ? '1' : '0'};
 height: ${props.isVisible ? '10rem' : '0rem'};
 transition-property: all;
@@ -41,6 +44,7 @@ transition-timing-function: ease;
 transition-delay: 0s;
 width: 60ch;
 max-width: 95%;
+margin: 1rem 0;
 `)
 
 
@@ -54,6 +58,7 @@ gap: 1rem;
 opacity: ${props.isVisible ? '1' : '0'};
 transition: opacity 0.4s ease;
 pointer-events: ${props.isVisible ? 'unset': 'none'};
+margin: 1rem 0;
 `)
 
 const StyledButton = styled(LoadingButton)(props => css`
@@ -120,6 +125,8 @@ function App() {
   const [destination, setDestination] = useState(null);
   const [destinationFullName, setDestinationFullName] = useState(null);
   const [destinationCountry, setDestinationCountry] = useState('us');
+  const [findingAirports, setFindingAirports] = useState(false)
+  const [finding, setFinding] = useState(null)
   const [results, setResults] = useState([]);
   const [roundTrip, setRoundTrip] = useState(false);
   const [loadingEmissions, setLoadingEmissions] = useState(false)
@@ -142,18 +149,16 @@ function App() {
       if (searchTerm !== '') {
         if (comboBox === 'origin') setOrigin(null)
         if (comboBox === 'destination') setDestination(null)
+        setFindingAirports(true)
         const response = await axios.get(`/api/search?q=${searchTerm}`);
         setResults(response.data.results.map((result, index) => ({...result, id: index})));
+        setFindingAirports(false)
       }
     } catch (error) {
+      setFindingAirports(false)
       console.error(error);
     }
   };
-
-  // Fetch airports when the component mounts
-  useEffect(() => {
-    handleSearch('', 'origin'); // Fetch airports for origin ComboBox
-  }, []);
 
   useEffect(() => {
     if (population !== null) {
@@ -191,7 +196,7 @@ function App() {
 
   const renderAirport = ((props, option, {inputValue}) => <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
     <Flag country={ option.iso } />
-    <HighlightedSubstring text={`${option.name} (${option.iata}) – ${getCountryName(option.iso)}`} query={inputValue} />
+    <HighlightedSubstring key={option.iata} text={`${option.name} (${option.iata}) – ${getCountryName(option.iso)}`} query={inputValue} />
   </Box>)
 
   const HighlightedSubstring = props => {
@@ -228,12 +233,17 @@ function App() {
       }
     }
   };
-
   return (
     <StyledMain>
-      <Typography variant="h4" component='h1'>
+      <Typography variant="h4" component='h1' gutterBottom>
         Environmental impact research
       </Typography>
+      <ReadableParagraph>
+        This is a research tool designed to estimate the carbon emissions generated during air travel. Its goal is to help people understand the environmental impact of flying and consider ways to reduce carbon emissions associated with air travel.
+      </ReadableParagraph>
+      <ReadableParagraph>
+        The carbon emission calculations are provided by <Link href="https://www.goclimate.com/">GoClimate (link opens in a new window)</Link>, based on calculation principles by <Link href="https://www.myclimate.org/en/information/about-myclimate/downloads/flight-emission-calculator/">myclimate (link opens in a new window)</Link>.
+      </ReadableParagraph>
       <StyledSection isVisible={resultsVisible}>
         <Card>
           <CardHeader
@@ -271,21 +281,47 @@ function App() {
           getOptionLabel={(option) => `${option.name} (${option.iata}) - ${getCountryName(option.iso)}`}
           value={results.find((option) => option.iata === origin) || null}
           onChange={handleOriginChange}
-          onInputChange={(_, newInputValue) => debounce(handleSearch(newInputValue, 'origin'), 500)}
+          onInputChange={(_, newInputValue) => handleSearch(newInputValue, 'origin')}
+          onOpen={() => {
+            setFinding('origin')
+            setResults([])
+          }}
+          onClose={() => setFinding(null)}
           renderInput={(params) => (
             <TextField
               {...params}
-              label="Origin airport"
+              label={<>Flying from</>}
               variant="outlined"
               fullWidth
-              sx={{ width: '100%' }} />
+              sx={{ width: '100%' }}
+              InputProps={
+                {...params.InputProps,
+                  startAdornment: (
+                    <>
+                      <FlightTakeoff color="disabled" />
+                      {params.InputProps.startAdornment}
+                    </>
+                  ),
+                  endAdornment: (
+                  <>
+                    {(findingAirports && finding === 'origin') ? <CircularProgress size={20} /> : null}
+                    {params.InputProps.endAdornment}
+                  </>
+                  )
+                }
+              }
+              />
           )}
           renderOption={renderAirport}
+          loading={findingAirports}
           autoHighlight
           autoSelect
           clearOnBlur={false}
           blurOnSelect
           selectOnFocus />
+        <Divider>
+            <Flight color='disabled' sx={{transform: 'rotate(90deg)'}} />
+        </Divider>
         <Autocomplete
           sx={{ width: '100%' }}
           options={results}
@@ -293,15 +329,39 @@ function App() {
           getOptionLabel={(option) => `${option.name} (${option.iata}) - ${getCountryName(option.iso)}`}
           value={results.find((option) => option.iata === destination) || null}
           onChange={handleDestinationChange}
-          onInputChange={(_, newInputValue) => debounce(handleSearch(newInputValue, 'destination'), 500)}
+          onInputChange={(_, newInputValue) => handleSearch(newInputValue, 'destination')}
+          onOpen={() => {
+            setFinding('destination')
+            setResults([])
+          }}
+          onClose={() => setFinding(null)}
           renderInput={(params) => (
             <TextField
               {...params}
-              label="Destination airport"
+              label={<>Flying to</>}
               variant="outlined"
-              fullWidth />
+              fullWidth 
+              InputProps={
+                {
+                  ...params.InputProps,
+                  startAdornment: (
+                    <>
+                      <FlightLand color="disabled" />
+                      {params.InputProps.startAdornment}
+                    </>
+                  ),
+                  endAdornment: (
+                    <>
+                      {(findingAirports && finding === 'destination') ? <CircularProgress size={20} /> : null}
+                      {params.InputProps.endAdornment}
+                    </>
+                  )
+                }
+              }
+            />
           )}
           renderOption={renderAirport}
+          loading={findingAirports}
           autoHighlight
           autoSelect
           clearOnBlur={false}
@@ -309,9 +369,10 @@ function App() {
           selectOnFocus />
         <FormControlLabel
           control={<Switch checked={roundTrip} onChange={(e) => setRoundTrip(e.target.checked)} />}
-          label="Round Trip" />
+          label="Round trip" />
         <StyledButton variant="outlined" loading={loadingEmissions} type="submit">Search</StyledButton>
       </StyledForm>
+
     </StyledMain>
   );
 }
